4584f7bf003d96224ad3f4bc9cc61188b0963922
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2018-12-29 17:10:36 +0200

    passing data up

--- lib/product_manager.dart
+++ lib/product_manager.dart
@@ -1,6 +1,7 @@
 import "package:flutter/material.dart";
 
 import "./products.dart";
+import './product_control.dart';
 
 class ProductManager extends StatefulWidget {
   final String startingProduct;
@@ -21,20 +22,18 @@ class _ProductManagerState extends State<ProductManager> {
     _products.add(widget.startingProduct);
   }
 
+  void _addProducts(String product) {
+    setState(() {
+      _products.add(product);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
         Container(
-            margin: EdgeInsets.all(10.0),
-            child: RaisedButton(
-                color: Theme.of(context).primaryColor,
-                child: Text('Add Product'),
-                onPressed: () {
-                  setState(() {
-                    _products.add('Advanced Food Tester');
-                  });
-                })),
+            margin: EdgeInsets.all(10.0), child: ProductControl(_addProducts)),
         Products(_products)
       ],
     );
