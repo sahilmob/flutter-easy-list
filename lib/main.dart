088e8afbdfcfd40e00c6cb5c6750db12e8bcbac5
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2018-12-29 13:41:42 +0200

    add app theme

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+          brightness: Brightness.light,
+          primarySwatch: Colors.blueGrey,
+          accentColor: Colors.blueAccent),
       home: Scaffold(
           appBar: AppBar(
             title: Text('EasyList'),
--- lib/product_manager.dart
+++ lib/product_manager.dart
@@ -28,6 +28,7 @@ class _ProductManagerState extends State<ProductManager> {
         Container(
             margin: EdgeInsets.all(10.0),
             child: RaisedButton(
+                color: Theme.of(context).primaryColor,
                 child: Text('Add Product'),
                 onPressed: () {
                   setState(() {
