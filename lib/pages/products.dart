7b4375a716cb4f89bbf9346b6b8674231994a7bc
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-01-05 14:21:51 +0200

    navigate from drawer item

--- lib/pages/products.dart
+++ lib/pages/products.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../product_manager.dart';
+import './product_admin.dart';
 
 class ProductsPage extends StatelessWidget {
   @override
@@ -14,7 +15,12 @@ class ProductsPage extends StatelessWidget {
               ),
               ListTile(
                 title: Text('Manage Products'),
-                onTap: () {},
+                onTap: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (BuildContext context) {
+                    return ProductAdmin();
+                  }));
+                },
               )
             ],
           ),
