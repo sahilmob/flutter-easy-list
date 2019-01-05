65278bd55249ebb83a22b49224dcf4e05cbe43ed
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-01-05 18:59:06 +0200

    add modal and dialog

--- lib/pages/product_create.dart
+++ lib/pages/product_create.dart
@@ -4,7 +4,16 @@ class ProductCreatePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text("Create Product"),
+      child: RaisedButton(
+        child: Text('Save'),
+        onPressed: () {
+          showModalBottomSheet(
+              context: context,
+              builder: (BuildContext context) {
+                return Center(child: Text('This is a modal'));
+              });
+        },
+      ),
     );
   }
 }
