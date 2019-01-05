6924ee5418e0e4d9bb9400d7213e0d562fd83ca4
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-01-05 13:24:25 +0200

    pass data back when clicking backbutton

--- lib/pages/product.dart
+++ lib/pages/product.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 
 class ProductPage extends StatelessWidget {
@@ -8,27 +9,32 @@ class ProductPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text(title),
-        ),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Image.asset(imageUrl),
-            Container(
-              child: Text(title),
-              padding: EdgeInsets.all(10.0),
+    return WillPopScope(
+        onWillPop: () {
+          Navigator.pop(context, false);
+          return Future.value(false);
+        },
+        child: Scaffold(
+            appBar: AppBar(
+              title: Text(title),
             ),
-            Container(
-              child: RaisedButton(
-                color: Theme.of(context).accentColor,
-                child: Text('Delete'),
-                onPressed: () => Navigator.pop(context, true),
-              ),
-              padding: EdgeInsets.all(10.0),
-            )
-          ],
-        ));
+            body: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Image.asset(imageUrl),
+                Container(
+                  child: Text(title),
+                  padding: EdgeInsets.all(10.0),
+                ),
+                Container(
+                  child: RaisedButton(
+                    color: Theme.of(context).accentColor,
+                    child: Text('Delete'),
+                    onPressed: () => Navigator.pop(context, true),
+                  ),
+                  padding: EdgeInsets.all(10.0),
+                )
+              ],
+            )));
   }
 }
