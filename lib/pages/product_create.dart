5d3481ea8cf55e1ed26dc7077f39e82a1e39556f
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-01-06 22:16:03 +0200

    add text inputs for the product title, description, and price

--- lib/pages/product_create.dart
+++ lib/pages/product_create.dart
@@ -1,21 +1,50 @@
 import 'package:flutter/material.dart';
 
-class ProductCreatePage extends StatelessWidget {
+class ProductCreatePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _ProudctCreatePageState();
+  }
+}
+
+class _ProudctCreatePageState extends State<ProductCreatePage> {
+  String titleValue;
+  String descriptionValue;
+  double priceValue;
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: RaisedButton(
-        child: Text('Save'),
-        onPressed: () {
-          showModalBottomSheet(
-              context: context,
-              builder: (BuildContext context) {
-                return Center(
-                  child: Text('This is a Modal!'),
-                );
-              });
-        },
-      ),
-    );
+    return Container(
+        margin: EdgeInsets.all(10.0),
+        child: Column(
+          children: <Widget>[
+            TextField(
+              decoration: InputDecoration(labelText: 'Product Title'),
+              onChanged: (String value) {
+                setState(() {
+                  titleValue = value;
+                });
+              },
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: 'Product Description'),
+              maxLines: 4,
+              onChanged: (String value) {
+                setState(() {
+                  descriptionValue = value;
+                });
+              },
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: 'Product Price'),
+              keyboardType: TextInputType.number,
+              onChanged: (String value) {
+                setState(() {
+                  priceValue = double.parse(value);
+                });
+              },
+            ),
+          ],
+        ));
   }
 }
