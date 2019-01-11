f95e31a433e4bd6a1fe5067dad64a248deca78db
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-01-11 18:40:41 +0200

    add background image

--- lib/pages/auth.dart
+++ lib/pages/auth.dart
@@ -19,7 +19,13 @@ class _AuthPageState extends State<AuthPage> {
         title: Text('Login'),
       ),
       body: Container(
-          margin: EdgeInsets.all(10.0),
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage('assets/background.jpg'),
+                  fit: BoxFit.cover,
+                  colorFilter: ColorFilter.mode(
+                      Colors.black.withOpacity(0.4), BlendMode.dstATop))),
+          padding: EdgeInsets.all(10.0),
           child: ListView(
             children: <Widget>[
               TextField(
