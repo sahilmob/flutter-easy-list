64528d7b2f56f7c6913d0c5e06b3bd6c2a5de33d
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-01-18 15:07:01 +0200

    add product favorite functunality

--- lib/scoped-models/products.dart
+++ lib/scoped-models/products.dart
@@ -4,13 +4,25 @@ import '../models/product.dart';
 class ProductsModel extends Model {
   List<Product> _products = [];
   int _selectedProductIndex;
+  bool _showFavorites = false;
 
   List<Product> get products {
     return List.from(_products);
   }
 
+  List<Product> get displaedProducts {
+    if (_showFavorites) {
+      return _products.where((Product product) => product.isFavorite).toList();
+    }
+    return List.from(_products);
+  }
+
   int get selectedProductIndex {
     return _selectedProductIndex;
+  }
+
+  bool get displayFavoritesOnly {
+    return _showFavorites;
   }
 
   Product get selectedProduct {
@@ -23,16 +35,39 @@ class ProductsModel extends Model {
   void addProduct(Product product) {
     _products.add(product);
     _selectedProductIndex = null;
+    notifyListeners();
   }
 
   void updateProduct(Product product) {
     _products[_selectedProductIndex] = product;
     _selectedProductIndex = null;
+    notifyListeners();
   }
 
   void deleteProduct() {
     _products.removeAt(_selectedProductIndex);
     _selectedProductIndex = null;
+    notifyListeners();
+  }
+
+  void toggleProductFavorateStatus() {
+    final Product selectedProduct = _products[_selectedProductIndex];
+    final bool isCurrentlyFavorite = selectedProduct.isFavorite;
+    final bool newFavoriteState = !isCurrentlyFavorite;
+    final Product updateProduct = Product(
+        title: selectedProduct.title,
+        description: selectedProduct.description,
+        price: selectedProduct.price,
+        image: selectedProduct.image,
+        isFavorite: newFavoriteState);
+    _products[_selectedProductIndex] = updateProduct;
+    notifyListeners();
+    _selectedProductIndex = null;
+  }
+
+  void toogleDisplayMode() {
+    _showFavorites = !_showFavorites;
+    notifyListeners();
   }
 
   void selectProduct(int index) {
